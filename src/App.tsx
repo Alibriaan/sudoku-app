480e6ecbcdd4ae571e74278bde6d3b3391f2c7be
d.sobolevskyi <d.sobolevskyi@viseven.com>
2022-12-26 21:52:25 +0200

    refactoring(App):

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Container } from '@mui/material';
+import { SudokuGame } from './containers/SudokuGame';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Container
+      className="App"
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+      }}
+    >
+      <SudokuGame />
+    </Container>
   );
 }
 
